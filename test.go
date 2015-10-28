56f210664e56ca2e182c9cd7f5620eb3c0a424fb
Finalreq <arminra7@gmail.com>
2015-10-27 21:45:26 -0700

    more testing

--- test.go
+++ test.go
@@ -7,16 +7,27 @@ type num struct{
 	y int
 }
 
+func initialize (n *num){
+//	n = &num{}
+	n.x = 5
+	n.y = 4
+	fmt.Println("this works")
+}
 
 func main (){
-	var x int
-	x = 3
-	var y *int
-	y = &x
+	//var x int
+	//x = 3
+	var y *num;
+	//y := &num{}
 	
-	n := num {nil, nil}
+	//n := num {nil, nil}
+	if y == nil{
+		y = &num {}
+		initialize (y)
+		fmt.Println("got it")
+		//return
+	}
 
-
-	fmt.Println("Hello World!", n.x, n.y)
+	fmt.Println("Hello World!", y.x, y.y)
 
 }	
