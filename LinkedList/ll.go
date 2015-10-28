1fe7d75c58ab6162b8af740c4303cdd072f07881
finalreq <arahimi1@eecs.wsu.edu>
2015-10-27 19:13:45 -0700

    not working properly yet

--- LinkedList/ll.go
+++ LinkedList/ll.go
@@ -7,20 +7,58 @@ type Node struct{
 	next *Node
 }
 
-func add_node (root *Node){
+func add_node (root *Node, nval int){
+	if root.val == nil{
+		root.val = nval
+		return
+	}
 	var tmp = root
-	tmp.val = 2
+	n := Node {nval, nil}
+	for tmp.next != nil{
+		tmp = tmp.next
+	}
+	tmp.next = &n
+	return
 
 }
+func print_list(root *Node){
+	var tmp = root
+	for tmp.next != nil{
+		fmt.Print(tmp.val, " --> ")
+		tmp = tmp.next
+	}
+	fmt.Println()
+	return
+}
+
 
 func main (){
 	root := Node{}
-	root.val = 1
+	var option int
+	var input int
+//	fmt.Print("Please enter a number to enter into the list: ")
+//	fmt.Scanln(&input);
+//	root.val = input
+//	root.next = nil
+	option = 0
+	for true{
+		if option == 1{
+			fmt.Print("Please enter a number to enter into the list: ")
+			fmt.Scanln(&input);
+			add_node(&root, input)
+		
+		} else if option == 2{
+			print_list(&root)
+		
+		} else if option == 3{
+			//remove number from list
+		
+		} 
 
-	fmt.Println("Hello world!", root.val)
+		fmt.Printf("1 ........ add new number \n2 ........ print the list \n3 ........ remove from the list \n")
 
-	add_node (&root)
-
-	fmt.Println(root.val)
+		fmt.Print("Please enter a new command: ")
+		fmt.Scanln(&option)
+	}
 	
 }
