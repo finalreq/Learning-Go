b56d97388ab91ab3e365f4c9a397da3222de0d83
finalreq <arahimi1@eecs.wsu.edu>
2015-10-28 23:18:21 -0700

    trying to figure this out.It's different from the way it works in c

--- LinkedList/ll.go
+++ LinkedList/ll.go
@@ -2,64 +2,87 @@ package main
 
 import "fmt"
 
-type Node struct{
-	val int
+type Node struct {
+	val  int
 	next *Node
 }
 
-func add_node (root *Node, nval int){
+func add_node(root *Node, nval int) {
 	var tmp = root
-	n := Node {nval, nil}
-	for tmp.next != nil{
+
+	n := Node{nval, nil}
+
+	for tmp.next != nil {
 		tmp = tmp.next
 	}
+
 	tmp.next = &n
 	return
 
 }
-func print_list(root *Node){
+func print_list(root *Node) {
 	var tmp = root
-	for tmp != nil{
+
+	for tmp != nil {
 		fmt.Print(tmp.val, " --> ")
 		tmp = tmp.next
 	}
+
 	fmt.Println()
 	return
 }
 
+func remove_node(root **Node, val int) {
+	var cur = root.next
+	var prev = **root
+	for cur.val != val {
+		if cur.next == nil {
+			fmt.Println("value does not exist in the list")
+			return
+		}
+		cur = cur.next
+		prev = *prev.next
+	}
+	prev.next = cur.next
+	prev = cur.next
+	cur = cur.next.next
+}
 
-func main (){
+func main() {
 	var root *Node
 	var option int
 	var input int
 
 	option = 0
 
-	for true{
+	for true {
 
-		if option == 1{
-			
+		if option == 1 {
+
 			fmt.Print("Please enter a number to enter into the list: ")
-			fmt.Scanln(&input);
-			
-			if root == nil{
+			fmt.Scanln(&input)
+
+			if root == nil {
 				root = &Node{input, nil}
-			}else{
+			} else {
 				add_node(root, input)
 			}
-		
-		} else if option == 2{
+
+		} else if option == 2 {
 			print_list(root)
-		
-		} else if option == 3{
+
+		} else if option == 3 {
+			fmt.Print("Please enter the number to be removed: ")
+			fmt.Scanln(&input)
+			remove_node(&root, input)
 			//TODO remove number from list
-		
-		} 
+
+		}
 
 		fmt.Printf("1 ........ add new number \n2 ........ print the list \n3 ........ remove from the list \n")
 
 		fmt.Print("Please enter a new command: ")
 		fmt.Scanln(&option)
 	}
-	
+
 }
--- test.go
+++ test.go
@@ -2,32 +2,40 @@ package main
 
 import "fmt"
 
-type num struct{
-	x int
-	y int
+type num struct {
+	x    int
+	next *num
 }
 
-func initialize (n *num){
-//	n = &num{}
-	n.x = 5
-	n.y = 4
-	fmt.Println("this works")
+func initialize(n **num) {
+	//	n = num{}
+	//n.x = 5
+	//n.y = 4
+	var tmp **num
+	//tmp := &num{}
+	tmp = n
+	//_ = tmp
+	//**n = *n.next
+	*tmp = *tmp.next
+	//fmt.Println("this works", *tmp.x)
 }
 
-func main (){
+func main() {
 	//var x int
 	//x = 3
-	var y *num;
-	//y := &num{}
-	
+
+	var y *num
+	var x *num
+	y = &num{5, nil}
+	x = &num{4, nil}
+
 	//n := num {nil, nil}
-	if y == nil{
-		y = &num {}
-		initialize (y)
-		fmt.Println("got it")
-		//return
-	}
+	y.next = x
 
-	fmt.Println("Hello World!", y.x, y.y)
+	fmt.Println(y.x)
 
-}	
+	initialize(&y)
+
+	fmt.Println(y.x)
+
+}
