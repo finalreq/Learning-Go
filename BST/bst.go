915a880456c84dffa6f3ecffb0fa9537d2b59b90
Finalreq <arminra7@gmail.com>
2015-11-03 23:54:26 -0800

    going to make a simple bst

---
+++ BST/bst.go
@@ -0,0 +1,34 @@
+// This will be a simple bst implementation in go
+
+package main
+
+import "fmt"
+
+type Node struct {
+	val   int
+	right *Node
+	left  *Node
+}
+
+func print_menu() {
+	option := 0
+	var input int
+
+	if option == 1 {
+	}
+}
+
+func main() {
+	root * Node
+	var option int
+	var input int
+
+	for true {
+
+		print_menu()
+
+	}
+
+	fmt.Println("Hello World!")
+
+}
